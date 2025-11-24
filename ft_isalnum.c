123eebc0333181ee2c14c4c1682becbd1b86e40f
Luciano Petillo <luciano.prnl@gmail.com>
2025-11-24 21:36:08 +0100

    test.c update, isalpha name function fixed

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,12 +6,12 @@
 /*   By: luciano <lupetill@student.42berlin.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 12:03:13 by luciano           #+#    #+#             */
-/*   Updated: 2025/11/24 09:20:48 by luciano          ###   ########.fr       */
+/*   Updated: 2025/11/24 21:03:43 by luciano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-int	isalnum(int c)
+int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
--- test.c
+++ test.c
@@ -6,19 +6,19 @@
 /*   By: lupetill <lupetill@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 14:42:39 by lupetill          #+#    #+#             */
-/*   Updated: 2025/11/24 09:43:45 by luciano          ###   ########.fr       */
+/*   Updated: 2025/11/24 21:32:25 by luciano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 #include <stdio.h>
 
-int	main(void)
+void test_alpha(void)
 {
 	char	*c;
-	int	a, i;
+	int	i;
 	
-	c = "abcdefghijklmnopqrstuvwxyz";
 	i = 0;
+	c = "abcdefghijklmnopqrstuvwxyz";
 	printf("#_ FUNTION isalpha:\n\tinputs -> 'a' 'b' 'c'...'z'\n");
 	while(c[i++])
 	{
@@ -26,41 +26,117 @@ int	main(void)
 			i++;
 		else
 		{
-			printf("FAIL IN -> %c\n", c[i]);
+			printf("\tFAIL IN -> %c\n", c[i]);
 			break;
 		}
 		if (!c[i])
-			printf("--PASS--\n");
-        }
-	c = "0 0123456789´?*#!&%712";
+			printf("\t--PASS--\n");        
+	}
 	i = 0;
+	c = "0 0123456789´?*#!&%712";
 	printf("\tinputs -> 0123456789´?*#!&712\n");
 	while(c[i++])
 	{
 		if (!c[i])
 		{
-			printf("--PASS--\n");
+			printf("\t--PASS--\n");
 			break;
 		}
 		if (!ft_isalpha(c[i]))
 			i++;
 		else
 		{
-			printf("FAIL IN -> %c\n", c[i]);
+			printf("\tFAIL IN -> %c\n", c[i]);
 			break;
 		}
-        }
-/*
-	if (ft_isdigit(c))
-                printf("ft_ isalpha -> %s = is digit\n", c);
-	c = "1";
-	a = *c;
-	if (ft_isalnum(a))
-                printf("ft_ isalnum -> %s = is alphanumeric\n", c);	
-	c = "a";	
-	a = *c;
-	if (ft_isalnum(a))
-                printf("ft_ isalnum -> %s = is alphanumeric\n", c);
-*/	return (0);
+       	}
+}
+
+void test_digit(void)
+{
+	char	*c;
+	int	i;
+	
+	i = 0;
+	c = "0123456789";
+	printf("#_ FUNTION isdigit:\n\tinputs -> '1' '2' '3'...'0'\n");
+	while(c[i++])
+	{
+		if (ft_isdigit(c[i]))
+			i++;
+		else
+		{
+			printf("\tFAIL IN -> %c\n", c[i]);
+			break;
+		}
+		if (!c[i])
+			printf("\t--PASS--\n");        
+	}
+	i = 0;
+	c = "abcdefghijklmnopqtuvwxyz´?*#!&";
+	printf("\tinputs -> abcdefghijklmnopqtuvwxyz´?*#!&\n");
+	while(c[i++])
+	{
+		if (!c[i])
+		{
+			printf("\t--PASS--\n");
+			break;
+		}
+		if (!ft_isdigit(c[i]))
+			i++;
+		else
+		{
+			printf("\tFAIL IN -> %c\n", c[i]);
+			break;
+		}
+       	}
+}
+
+void test_alnum(void)
+{
+	char	*c;
+	int	i;
+	
+	i = 0;
+	c = "01d2b34a56cz789";
+	printf("#_ FUNTION isalnum:\n\tinputs -> 01d2b34a56cz789\n");
+	while(c[i])
+	{
+		if (ft_isalnum(c[i]))
+			i++;
+		else
+		{
+			printf("\tFAIL IN -> %c\n", c[i]);
+			break;
+		}
+		if (!c[i])
+			printf("\t--PASS--\n");        
+	}
+	i = 0;
+	c = "();,.§´?*#!&";
+	printf("\tinputs -> ();,.§´?*#!&\n");
+	while(c[i])
+	{
+		if (!ft_isalnum(c[i]))
+			i++;
+		else
+		{
+			printf("\tFAIL IN -> %c\n", c[i]);
+			break;
+		}
+		if (!c[i])
+		{
+			printf("\t--PASS--\n");
+			break;
+		}
+       	}
+}
+
+int	main(void)
+{
+	test_alpha();
+	test_digit();
+	test_alnum();
+	return (0);
 }
 
